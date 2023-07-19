826cc95b5eb477772744827177577455cdeac609
--replace <david.thuku@studentmoringa.school.com>
2023-07-19 17:19:48 +0300

    done with assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,18 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+
+  const inlineStyles = {
+    color: "firebrick",
+  };
+
+  return (
+    <div id="home">
+      <h1 style={inlineStyles}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
